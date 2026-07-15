9ad0bb176842190e3388f10c4b45f1493e13882d
Ayush7870-web <ayushkr1919@gmail.com>
2026-07-15 06:21:48 +0530

    Add files via upload

---
+++ shutdown_or_restart_your_device.py
@@ -0,0 +1,31 @@
+from logging import shutdown
+import os
+import platform
+
+def shutdown_device():
+    """
+    shutdown the system based on the user advice"""
+
+    if platform.system() == "Windows":
+        os.system("shutdown -s")
+    elif platform.system() == "Linux" or platform.system() == "Darwin":
+        os.system("shutdown -h now")
+    else:
+        print("os not supported")
+
+def restart():
+    if platform.system() == "Windows":
+        os.system("shutdown -t 0 -r -f")
+    elif platform.system() == "Linux" or platform.system() == "Darwin":
+        os.system('reboot now')
+    else:
+        print("Os not supported!")
+
+command = input("Use \'r\' for restart and \'s\' for shutdown: ").lower()
+
+if command == "r":
+    restart()
+elif command == "s":
+    shutdown()
+else:
+    print("Wrong letter")
